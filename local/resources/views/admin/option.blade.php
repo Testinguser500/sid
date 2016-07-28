45b57f9cc3f03f2a230026f56c304fa1032335fd
testinguser <testinguser500@gmail.com>
2016-07-28 17:45:15 +0530

    @optionchanges

--- local/resources/views/admin/option.blade.php
+++ local/resources/views/admin/option.blade.php
@@ -128,7 +128,7 @@
 		  <div class="help-block"></div>
                 </div> 
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Answer</label>
+                  <label for="exampleInputEmail1">Option Name</label>
 		  <input type="text" class="form-control" id=""  ng-model="option.option_value" name="option_value" placeholder="Option Value" >
 		  <div class="help-block"></div>
                 </div>                
