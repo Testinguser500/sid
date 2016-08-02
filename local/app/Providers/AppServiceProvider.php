f9f84f83ea502240973f92c51509cf7d3f6881fc
Testinguser500 <Testinguser500@gmail.com>
2016-08-02 12:06:00 +0530

    validation in option

--- local/app/Providers/AppServiceProvider.php
+++ local/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Validator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,7 +14,73 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+//           Validator::extend('is_unique_with', function($attribute, $value, $parameters, $validator) {
+//
+//            if(!empty($value) && (strlen($value) % 2) == 0){
+//
+//                return true;
+//
+//            }
+//
+//                return false;
+//
+//        });
+        Validator::extend('soft_composite_unique', function ($attribute, $value, $parameters, $validator) {
+            // Custom validation logic
+
+            // remove first parameter and assume it is the table name
+            $table = array_shift( $parameters ); 
+
+            // start building the conditions
+           // $fields = [ $attribute => $value ]; // current field, company_code in your case
+            $ar= array_shift( $parameters ); 
+            $fields[] =array($ar,$value) ;
+            // iterates over the other parameters and build the conditions for all the required fields
+            while ( $field = array_shift( $parameters ) ) {
+                if (strpos($field, '=') !== false) {
+                    $fd=explode('=',$field);
+                    $fields[]  =array($fd[0],$fd[1]) ;
+                }  else {
+                     $fields[]  =array('id','!=',$field) ;
+                }
+               
+            }
+           $fields[]=array('is_delete','0');
+            // query the table with all the conditions
+            $result = \DB::table( $table )->select( \DB::raw( 1 ) )->where( $fields )->first();
+          
+            return empty( $result );
+        }, 'This value :attribute already exists!');
+        
+          Validator::extend('soft_unique', function ($attribute, $value, $parameters, $validator) {
+            // Custom validation logic
+
+            // remove first parameter and assume it is the table name
+            $table = array_shift( $parameters ); 
+
+            // start building the conditions
+           // $fields = [ $attribute => $value ]; // current field, company_code in your case
+           
+            $fields[] =array($attribute,$value) ;
+            // iterates over the other parameters and build the conditions for all the required fields
+            while ( $field = array_shift( $parameters ) ) {
+                if (strpos($field, '=') !== false) {
+                    $fd=explode('=',$field);
+                    $fields[]  =array($fd[0],$fd[1]) ;
+                }  else {
+                     $fields[]  =array('id','!=',$field) ;
+                }
+               
+            }
+           $fields[]=array('is_delete','0');
+           $fields[]=array('parent_id','0');
+            // query the table with all the conditions
+            $result = \DB::table( $table )->select( \DB::raw( 1 ) )->where( $fields )->first();
+          
+            return empty( $result );
+        }, 'This value :attribute already exists!');
     }
+    
 
     /**
      * Register any application services.
